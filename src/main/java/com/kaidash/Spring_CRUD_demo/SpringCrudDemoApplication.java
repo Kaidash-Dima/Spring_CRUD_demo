2c9f746a3a7ea8337019532f43bfa61d94c00567
Kaidash-Dima <17254dima@gmail.com>
2022-04-24 22:30:00 +0200

    Initial commit

---
+++ src/main/java/com/kaidash/Spring_CRUD_demo/SpringCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kaidash.Spring_CRUD_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaidash/Spring_CRUD_demo/SpringCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaidash.Spring_CRUD_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
